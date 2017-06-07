f0680f9407d666b0def3e93343f5b91af9dd159b
ayato_p <ayato.skycrawlers@gmail.com>
2017-06-07 14:21:51 +0900

    Remove unilog from dev dependencies

--- project.clj
+++ project.clj
@@ -13,10 +13,9 @@
   :dependencies [[clj-http "3.5.0"]
                  [cheshire "5.7.1"]]
   :profiles
-  {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
-                        [spootnik/unilog "0.7.20"]]
-         :injections [(require '[unilog.config :as uni])
-                      (uni/start-logging! {:level :debug :console true})]}
+  {:dev {:resource-paths ["env/dev/resources"]
+         :dependencies [[org.clojure/clojure "1.8.0"]
+                        [rkworks/baum "0.4.0"]]}
    :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
    :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
    :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
