661701bf9ad8070e4e66d8b1394d3c41c5445920
ayato_p <ayato.skycrawlers@gmail.com>
2017-06-07 17:48:21 +0900

    Add records API

---
+++ src/cybozu_http/api/kintone/records.clj
@@ -0,0 +1,22 @@
+(ns cybozu-http.api.kintone.records
+  (:refer-clojure :exclude [get])
+  (:require [cybozu-http.api.kintone.bare :refer [defapi]]))
+
+(defapi get :get "/records.json"
+  [app :- app-id]
+  [fields     :- fields
+   query      :- query
+   totalCount :- total-count])
+
+(defapi post :post "/records.json"
+  [app     :- app-id
+   records :- records])
+
+(defapi put :put "/records.json"
+  [app     :- app-id
+   records :- records])
+
+(defapi delete :delete "/records.json"
+  [app :- app-id
+   ids :- record-ids]
+  [revisions :- revisions])
