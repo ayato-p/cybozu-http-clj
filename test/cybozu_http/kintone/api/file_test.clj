b84d6a8157cc501d5ca0ebb72add2b06df37bc1b
liquidz <liquidz.uo@gmail.com>
2017-12-05 15:50:25 +0900

    Add option to download file as byte-array

--- test/cybozu_http/kintone/api/file_test.clj
+++ test/cybozu_http/kintone/api/file_test.clj
@@ -10,16 +10,25 @@
 (reset-meta! *ns* {})
 (t/use-fixtures :once (h/wrap-setup db))
 
+(defn- byte-array? [x]
+  (= (type x)
+     (Class/forName "[B")))
+
 (t/deftest upload-download-test
-  (t/testing "file upload test"
-    (let [{:keys [auth app-id]} @db
-          file (io/file (io/resource "upload-test-file.org"))
-          upload-res (f/upload auth file)
-          {id :id} (->> (assoc-in h/record ["attachment_file" :value 0 :fileKey] upload-res)
-                        (hash-map :record)
-                        (r/post auth app-id))
-          res (r/get auth app-id id)
-          download-res (f/download* auth (get-in res [:attachment_file :value 0 :fileKey]))]
+  (let [{:keys [auth app-id]} @db
+        file (io/file (io/resource "upload-test-file.org"))
+        upload-res (f/upload auth file)
+        {id :id} (->> (assoc-in h/record ["attachment_file" :value 0 :fileKey] upload-res)
+                      (hash-map :record)
+                      (r/post auth app-id))
+        res (r/get auth app-id id)
+        file-key (get-in res [:attachment_file :value 0 :fileKey])
+        download-res (f/download* auth file-key)]
+    (t/testing "file upload test"
       (t/is (string? upload-res))
       (t/is (string? (:body download-res)))
-      (t/is (= (:body download-res) (slurp file))))))
+      (t/is (= (:body download-res) (slurp file))))
+
+    (t/testing "download as byte-array test"
+      (let [download-res (f/download* auth file-key :as-byte-array? true)]
+        (t/is (byte-array? (:body download-res)))))))
